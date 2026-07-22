a22aa58f9d485beea5cb6b617b4cf9cd1e621528
Wawat Prigala <wawat.prigala@pfigroups.com>
2026-07-22 18:02:48 +0700

    feat: enforce WooCommerce registration settings on init to ensure consistent environment configuration

--- wp-content/themes/canopy-child/functions.php
+++ wp-content/themes/canopy-child/functions.php
@@ -8,3 +8,19 @@ add_action( 'wp_enqueue_scripts', function () {
 		wp_enqueue_style( 'canopy-child-custom', "$uri/custom.css", array( 'canopy-child' ), filemtime( "$dir/custom.css" ) );
 	}
 }, 99 );
+
+/**
+ * Ensure WooCommerce registration settings are properly configured across environments.
+ */
+add_action( 'init', function () {
+	if ( 'yes' !== get_option( 'woocommerce_enable_myaccount_registration' ) ) {
+		update_option( 'woocommerce_enable_myaccount_registration', 'yes' );
+	}
+	if ( 'no' !== get_option( 'woocommerce_registration_generate_password' ) ) {
+		update_option( 'woocommerce_registration_generate_password', 'no' );
+	}
+	if ( 'yes' !== get_option( 'woocommerce_registration_generate_username' ) ) {
+		update_option( 'woocommerce_registration_generate_username', 'yes' );
+	}
+} );
+
